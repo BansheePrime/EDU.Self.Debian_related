2b0cdebd38d9e3959c94d9546cceea875b8dcb17
gidra <gidra@rukia.dev.local>
2024-03-03 14:32:55 +0300

    typescript experiment. moving on

--- krause_developer/sample-express/routes.ts
+++ krause_developer/sample-express/routes.ts
@@ -1,20 +1,32 @@
 import fetch from "node-fetch";
 
-const routeHello = () => "Hello World!";
+const routeHello = (): string => "Hello World!";
 
-const routeAPINames = async () => {
+const routeAPINames = async (): Promise<string> => {
 	const url = "https://www.usemodernfullstack.dev/api/v1/users";
-	let data;
+	let data: responseItemType[];
 	try {
 		const response = await fetch(url);
-		data = await response.json();
+		data = (await response.json()) as responseItemType[];
 	} catch (err) {
-		return err;
+		return "Error";
 	}
 	const names = data
 		.map((item) => `id: ${item.id}, name: ${item.name}`)
 		.join("<br>");
 	return names;
 };
-export { routeHello, routeAPINames };
+
+const routeWeather = (query: WeatherQueryInterface): WeatherDetailType =>
+	queryWeatherData(query);
+
+const queryWeatherData = (query: WeatherQueryInterface): WeatherDetailType => {
+	return {
+		zipcode: query.zipcode,
+		weather: "sunny",
+		temp: 35
+	};
+};
+
+export { routeHello, routeAPINames, routeWeather };
 
